40745d6d33096a4cd0436dbb465d1d0f5be9a5a9
nsikak essien <nsikakessien66@gmail.com>
2023-09-14 09:38:53 +0100

    set environment variable

--- src/dotenv.ts
+++
@@ -1,6 +0,0 @@
-// dotenv.js
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-// const API_KEY = "178f7aafed9b85876e9786c5d64e8b7e";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   params: {
-    api_key: import.meta.env.VITE_API_KEY,
+    api_key: import.meta.env.VITE_REACT_APP_API_KEY,
   },
 });
 
